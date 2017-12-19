e5962beb1ad2cc122974708ca3662a3f7a47a0c4
rasmusjesperjessen <raje16ab@student.cbs.dk>
2017-12-19 13:15:28 +0100

    DeleteUser færdig

--- js/login.js
+++ js/login.js
@@ -8,11 +8,16 @@ $(document).ready(() => {
     const password = $("#inputPassword").val();
 
     SDK.User.login(username, password, (err, data) => {
-        if(err && err.xhr.status === 401)
-      {
-    $(".form-group").addClass("has-error");
+       if (!username || !password) {
+           alert("You cannot login without entering any information")
+       }
+       else if(err && err.xhr.status === 401) {
+            $(".form-group").addClass("has-error");
       } else if (err) {
-    console.log("Something went wrong")
+           console.log("Something went wrong");
+           alert("You have entered wrong username or password.");
+           $("#inputUsername").val("");
+           $("#inputPassword").val("");
       } else {
           window.alert("Login var succesfuld :-)");
           window.location.href = "my-page.html";
--- js/my-page.js
+++ js/my-page.js
@@ -1,10 +1,45 @@
 $(document).ready(() => {
 
+
+
   SDK.User.loadNav();
 
-$("#adminReg-button").hide();
+    const currentUser = SDK.User.current();
+
+    const deleteUserButton = $("#deleteUser-button");
+    const adminRegButton = $("#adminReg-button");
+    const createQuizButton = $("#createQuiz-button");
+
+adminRegButton.hide();
     if (SDK.Storage.load("type") === 2) {
         $('#adminReg-button').show();
     }
+
+createQuizButton.hide();
+    if (SDK.Storage.load("type") === 2) {
+        $('#createQuiz-button').show();
+    }
+
+    deleteUserButton.click (() => {
+
+        const deleteUserId = currentUser.userId;
+
+        if (confirm("Do you wish to delete your user?"))
+
+        {
+            SDK.User.delete(deleteUserId, (err) => {
+
+
+            });
+
+            SDK.User.logOut();
+
+        }
+        else {
+            alert("User was not deleted!")
+        }
+
+    });
+
 });
 
